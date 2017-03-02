943ec63b4271eca3551e66bb7131f43f2db99685
zhangcunxin <zcunxin@163.com>
2017-03-02 20:15:33 +0800

    add blackcat

--- src/main/java/com/github/bingoohuang/springrest/boot/RestConfiguration.java
+++ src/main/java/com/github/bingoohuang/springrest/boot/RestConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.bingoohuang.springrest.boot;
 
+import com.github.bingoohuang.blackcat.instrument.spring.BlackcatInterceptor;
 import com.github.bingoohuang.springrest.boot.interceptor.SignInterceptor;
 import com.github.bingoohuang.springrest.boot.interceptor.ThreadLocalInterceptor;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +12,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan
 public class RestConfiguration extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
+        if (HasBlackcat) {
+            registry.addInterceptor(new BlackcatInterceptor());
+        }
         registry.addInterceptor(new SignInterceptor());
         registry.addInterceptor(new ThreadLocalInterceptor());
     }
+
+    public static boolean classExists(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (Throwable e) { // including ClassNotFoundException
+            return false;
+        }
+    }
+
+    public static final boolean HasBlackcat = classExists(
+            "com.github.bingoohuang.blackcat.instrument.callback.Blackcat");
 }
